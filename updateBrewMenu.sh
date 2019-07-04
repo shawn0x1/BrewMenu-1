8e091a919c916c83b4f85cee943973b72b17c493
VicerExciser <acondict11@gmail.com>
2019-07-04 01:44:06 -0400

    removed non-user permission change calls

--- updateBrewMenu.sh
+++ updateBrewMenu.sh
@@ -6,16 +6,8 @@ fi
 sudo -u halfway git clone https://github.com/VicerExciser/BrewMenu.git
 sleep 3
 
-#mv -f /home/halfway/BrewMenu/BrewMenu/* /home/halfway/BrewMenu/
-#rm -rf /home/halfway/BrewMenu/BrewMenu
 
-#if [ ! -d "/home/halfway/BrewMenu/ascii_art" ]; then
-#	mkdir /home/halfway/BrewMenu/ascii_art
-#fi
-chmod -R a+wrx /home/halfway/BrewMenu/
-chown -R halfway /home/halfway/BrewMenu/
-chgrp -R users /home/halfway/BrewMenu/
+# chmod -R a+wrx /home/halfway/BrewMenu/
+# chown -R halfway /home/halfway/BrewMenu/
+# chgrp -R users /home/halfway/BrewMenu/
 
-#/usr/bin/python3 /home/halfway/BrewMenu/setup.py
-#menu () { cd /home/halfway/BrewMenu; }
-#menu
