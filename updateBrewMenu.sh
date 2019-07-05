f84716925b9d7e188b262e5f6c07e44039c6101d
VicerExciser <acondict11@gmail.com>
2019-07-05 02:07:48 -0400

    cleaned update script

--- updateBrewMenu.sh
+++ updateBrewMenu.sh
@@ -1,20 +1,15 @@
-#svn export https://github.gatech.edu/llerner3/vip/branches/spring2019/brewery/BrewMenu --force
-#/usr/bin/python3 BrewMenu/setup.py
+#!/bin/bash
+sleep 3
 if nc -dzw1 github.com 443 && echo |openssl s_client -connect github.com:443 2>&1 |awk '
   handshake && $1 == "Verification" { if ($2=="OK") exit; exit 1 }
   $1 $2 == "SSLhandshake" { handshake = 1 }'
 then
-  echo "we have connectivity..."
+  echo "[ We have connectivity... ]"
   if [ -d "/home/halfway/BrewMenu" ]; then
 	rm -rf /home/halfway/BrewMenu
   fi
   git clone https://github.com/VicerExciser/BrewMenu.git
   sleep 3
 else
-  echo "github host unreachable: update BrewMenu was unsuccessful."
+  echo "[[<ERROR :: GitHub host unreachable: Update BrewMenu was unsuccessful.>]]"
 fi
-
-# chmod -R a+wrx /home/halfway/BrewMenu/
-# chown -R halfway /home/halfway/BrewMenu/
-# chgrp -R users /home/halfway/BrewMenu/
-
